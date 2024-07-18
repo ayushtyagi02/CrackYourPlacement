01b911adaf9b06f7e290d7506c5fc9c1a2e7130d
ayushtyagi02 <tayush139@gmail.com>
2024-07-18 19:17:06 +0530

    Day 2

---
+++ Arrays/StocksBuySell.cpp
@@ -0,0 +1,21 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int buyprice=Integer.MAX_VALUE;
+//         int sellprice=0;
+//         int maxProfit=0;
+//         buyprice=prices[0];
+//         for(int i=1;i<prices.length;i++){
+//             sellprice=prices[i];
+
+//             if(buyprice<sellprice){
+//                  int Profit=sellprice-buyprice;
+//                 maxProfit=Math.max(Profit,maxProfit);
+//             }
+//             else{
+//                 buyprice=sellprice;
+//             }
+//         }
+        
+//         return maxProfit;
+//     }
+// }
